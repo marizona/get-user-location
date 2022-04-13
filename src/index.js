c8a84225d9152eeba30a08836621d5f7c35e81fd
marizona <marie.jacquier@epitech.eu>
2022-04-13 14:13:11 +0200

    handling errors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-
 import { createRoot } from "react-dom/client";
+import { Component } from "react";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.log(err)
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //state initialized, the only time we do direct assignment
+      lat: null,
+      errorMessage: "",
+    };
 
-  return <div>hi there</div>;
-};
-
-root.render(<App/>)
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude }); //we always update state with setState()
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    } else if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    } else return <div>Loading!</div>;
+  }
+}
+root.render(<App />);
