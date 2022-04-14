8b76145c8ab5d9809f6d780d0a9436e20a70a88a
marizona <marie.jacquier@epitech.eu>
2022-04-14 09:38:03 +0200

    fix error

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React from 'react';
 
-export default function SeasonDisplay() {
-  return (
-    <div>Season Display</div>
-  )
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach",
+    iconName: "☀️",
+  },
+  winter: {
+    text: "It is chilly",
+    iconName: "❄️",
+  },
+};
+
+const getSeason = (lat, month)=>{
+if(month > 2 && month < 9){
+    return lat > 0 ? 'summer':'winter';
+}else{
+    return lat > 0 ? 'winter' : 'summer';
+}
 }
 
+ function SeasonDisplay(props) {
+    const season = getSeason(props.lat, new Date().getMonth())
+    const {text, iconName} = seasonConfig[season]
+
+    
+
+  return (
+    <div>
+      <i className="icon">{`${iconName}`}</i>
+      <h1>{text}</h1>
+      <i className="icon">{`${iconName}`}</i>
+    </div>
+  );
+}
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,29 @@
 import { createRoot } from "react-dom/client";
 import { Component } from "react";
+import SeasonDisplay from "./SeasonDisplay";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+state = {
       //state initialized, the only time we do direct assignment
       lat: null,
       errorMessage: "",
     };
 
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude }); //we always update state with setState()
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
+    
+    componentDidMount() {
+      window.navigator.geolocation.getCurrentPosition(
+        (position) => this.setState({ lat: position.coords.latitude }), //we always update state with setState()
+        (err) => this.setState({ errorMessage: err.message })
+      );
       }
-    );
-  }
+  
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     } else if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat}/>
     } else return <div>Loading!</div>;
   }
 }
